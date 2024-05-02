f057594e0ea920e218b747c5716d61c470054ce3
Sahil Ranout <mrwebbeast@gmail.com>
2024-05-02 13:09:42 +0530

    updates

---
+++ example/lib/app/config.dart
@@ -0,0 +1,15 @@
+List<String> videoUrls = [
+  "https://cdn.pixabay.com/video/2016/04/18/2849-163375551_large.mp4",
+  "https://cdn.pixabay.com/video/2023/08/22/177223-857013274_large.mp4",
+  "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4",
+  "https://api.gtp.proapp.in/api/v1/fetch-api-aws-file?path=private/feeds/video/video_24041503193256.MP4",
+  "https://cdn.pixabay.com/video/2018/03/22/15153-261472292_large.mp4",
+  "https://cdn.pixabay.com/video/2020/08/02/46216-447422820_large.mp4",
+  "https://assets.mixkit.co/videos/preview/mixkit-spinning-around-the-earth-29351-large.mp4"
+      "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+  "https://cdn.pixabay.com/video/2021/04/09/70463-538463758_large.mp4",
+  "https://cdn.pixabay.com/video/2017/06/11/9794-221163266_large.mp4",
+  "https://cdn.pixabay.com/video/2022/05/22/117745-713036088_large.mp4",
+  "https://assets.mixkit.co/videos/preview/mixkit-daytime-city-traffic-aerial-view-56-large.mp4",
+  "https://assets.mixkit.co/videos/preview/mixkit-spinning-around-the-earth-29351-large.mp4",
+];
